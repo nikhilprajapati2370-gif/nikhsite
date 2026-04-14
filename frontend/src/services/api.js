3100d04cd399489481217c1235fc3b3f9f79387e
https-bittcode.github.io- <nikhilprajapati2370@gmail.com>
2026-04-14 20:51:41 +0530

    Implement request interceptor for API calls
    Add request interceptor to include authorization token

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,6 +7,15 @@ const api = axios.create({
   headers: { "Content-Type": "application/json" },
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+
 api.interceptors.response.use(
   (response) => response,
   (error) => {
