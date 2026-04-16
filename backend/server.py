9a7cb33d9a2f618e6ca400c7db025d95ac78e9b1
https-bittcode.github.io- <nikhilprajapati2370@gmail.com>
2026-04-16 18:58:05 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -189,8 +189,9 @@ class OrderStatusUpdate(BaseModel):
 @api_router.post("/auth/forgot-password")
 async def forgot_password(data: ForgotPasswordRequest):
     email = data.email.lower().strip()
+     print("OTP requested for:", email)
     user = await db.users.find_one({"email": email})
-    print("OTP requested for:", email)
+   
 
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
